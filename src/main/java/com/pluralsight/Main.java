7ce7887fa949da29102c80de76819b37aab81c60
Alexander Diaz <adiaz1@my.yearupunited.org>
2025-11-26 09:54:05 -0800

    first commit

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("My fun fact:I can eat a lot");
+        System.out.println("TODO: replace this text in a Git branch.");
+    }
+}
